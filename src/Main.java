306e20429c91ee4479593a3267e080bddefda434
Willian Mateus <willianmateussan@gmail.com>
2023-02-20 13:16:08 -0300

    end of the project

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -21,9 +23,39 @@ public class Main {
         mentoria.setData(LocalDate.now());
 
 
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descricao Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+        Dev dev1 = new Dev();
+        dev1.setNome("Willian Mateus");
+        dev1.increverBootcamp(bootcamp);
+        System.out.println("Conteúdos disponíveis para " + dev1.getNome() + ": " + dev1.getConteudosInscritos());
+        dev1.progredir();
+        dev1.progredir();
+        dev1.progredir();
+        System.out.println("==");
+        System.out.println("Conteúdos concluídos por " + dev1.getNome() + ": " + dev1.getConteudosConcluidos());
+        System.out.println("Conteúdos disponíveis para " + dev1.getNome() + ": " + dev1.getConteudosInscritos());
+        System.out.println("XP: " + dev1.calcularTotalXp());
+
+        System.out.println("========================");
+
+        Dev dev2 = new Dev();
+        dev2.setNome("Sara Muller");
+        dev2.increverBootcamp(bootcamp);
+        System.out.println("Conteúdos disponíveis para " + dev2.getNome() + ": " + dev2.getConteudosInscritos());
+        dev2.progredir();
+        System.out.println("==");
+        System.out.println("Conteúdos concluídos por " + dev2.getNome() + ": " + dev2.getConteudosConcluidos());
+        System.out.println("Conteúdos disponíveis para " + dev2.getNome() + ": " + dev2.getConteudosInscritos());
+        System.out.println("XP: " + dev2.calcularTotalXp());
+
+
+
+
     }
 }
